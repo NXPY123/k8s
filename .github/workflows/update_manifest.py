d5e2ac5ab0073ea3eb958ddb667fe7e2d9215b7b
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-05-15 09:24:48 +0200

    First commit

--- .github/workflows/update_manifest.py
+++ .github/workflows/update_manifest.py
@@ -16,7 +16,9 @@ def update_image_version(yaml_file, new_version):
     with open(yaml_file, 'w') as file:
         yaml.dump(yaml_data, file, default_flow_style=False)
 
-# Usage example
-yaml_file = "/yaml/testapp-rollout.yaml"
+# Get root directory
+root_dir = os.getcwd()
+print("Root directory: " + root_dir)
+yaml_file = root_dir + "/yaml/testapp-rollout.yaml"
 new_version = os.getenv('GITHUB_SHA')
 update_image_version(yaml_file, new_version)
